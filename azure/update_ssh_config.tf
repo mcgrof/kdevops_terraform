8b40a3508219526c59890ab62c5dc23419b3ce8b
Luis Chamberlain <mcgrof@kernel.org>
2020-07-25 16:07:47 -0600

    azure: remove duplicate resource for ansible configuration
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- azure/update_ssh_config.tf
+++ azure/update_ssh_config.tf
@@ -1,13 +1,3 @@
-data "template_file" "ansible_cmd" {
-  template = "${file("ansible_provision_cmd.tpl")}"
-  vars {
-    inventory = "../../${var.ansible_inventory}"
-    playbook_dir = "../../${var.ansible_playbookdir}/"
-    provision_playbook = "${var.ansible_provision_playbook}"
-    extra_args = "--extra-vars='data_home_dir=/home/${var.ssh_config_user}'"
-  }
-}
-
 locals {
   limit_count = var.ssh_config_update != "true" ? 0 : local.num_boxes
   shorthosts = azurerm_linux_virtual_machine.kdevops_vm.*.name
