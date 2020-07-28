95f8a320df6d5d78e134894273c58635f200ebea
Luis Chamberlain <mcgrof@kernel.org>
2020-07-28 01:13:23 -0600

    update openstack provider
    This is the latest release.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- openstack/provider.tf
+++ openstack/provider.tf
@@ -2,7 +2,7 @@
 
 provider "openstack" {
   # any non-beta version >= 1.19.0 and < 1.20.0, e.g. 1.19.1
-  version = "~>1.19.0"
+  version = "~>1.29.0"
 
   # We prefer this method as it means you also get to use standard openstack
   # utilities that also use and share the same configuration files.
