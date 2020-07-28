16a6d06a56f08b0ee735ad2726f59e794d387284
Luis Chamberlain <mcgrof@kernel.org>
2020-07-27 22:01:25 -0600

    gce: enable ansible provisioner
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- gce/update_ssh_config.tf
+++ gce/update_ssh_config.tf
@@ -20,3 +20,10 @@ module "ssh_config_update_host_entries" {
   use_backup = var.ssh_config_backup != "true" || var.ssh_config == "/dev/null" ? "" : "true"
   backup_postfix = "kdevops"
 }
+
+resource "null_resource" "ansible_call" {
+  provisioner "local-exec" {
+    command = local.ansible_cmd
+  }
+  depends_on = [ module.ssh_config_update_host_entries ]
+}
