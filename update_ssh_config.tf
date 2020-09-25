a784242100cea650a65da628bcac395148278e0f
Luis Chamberlain <mcgrof@kernel.org>
2020-09-24 19:40:33 -0600

    update_ssh_config.tf: remove local variable definitions
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- update_ssh_config.tf
+++ update_ssh_config.tf
@@ -1,17 +1,3 @@
-locals {
-  limit_count = var.ssh_config_update != "true" ? 0 : local.num_boxes
-  all_tags = aws_instance.kdevops_instance.*.tags
-  shorthosts = [
-    for tags in local.all_tags:
-      format("%s", lookup(tags, "Name"))
-  ]
-  all_ipv4s = aws_eip.kdevops_eip.*.public_ip
-  ipv4s = [
-    for ip in local.all_ipv4s:
-      ip == "" ? "0.0.0.0" : ip
-  ]
-}
-
 module "ssh_config_update_host_entries" {
   source  = "mcgrof/add-host-ssh-config/kdevops"
   version = "2.1.2"
